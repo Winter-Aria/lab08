6ea15fc125db5dfb8f1ceeee2c0b14184c88e02f
Aria <notariaepic@gmail.com>
2025-04-28 13:44:20 +1000

    done

--- login.php
+++ login.php
@@ -1,18 +1,11 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
+
 <?php include 'header.php' ?>
     <main>
     <form method = "post" action = "process.php">
-        <label for="username">Username:</label>
+        Username:
         <input type ="text" name="username" required><br>
 
-        <label for = "password"> Password:</label>
+         Password:
         <input type ="password" name="password" required><br>
 
         <input type="hidden" name="token " value="abc123">
