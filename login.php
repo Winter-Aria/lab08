1b7736aa7b312ed8c65e8b9ecfaae14f83945da3
Aria <notariaepic@gmail.com>
2025-04-28 13:37:00 +1000

    commit

--- login.php
+++ login.php
@@ -10,10 +10,10 @@
     <main>
     <form method = "post" action = "process.php">
         <label for="username">Username:</label>
-        <input type ="text" name "username" required><br>
+        <input type ="text" name="username" required><br>
 
         <label for = "password"> Password:</label>
-        <input type ="password" name "password" required><br>
+        <input type ="password" name="password" required><br>
 
         <input type="hidden" name="token " value="abc123">
         <input type="submit" value="Login">
